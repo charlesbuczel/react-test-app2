035b1221abf46979ace4e9f6cfb2188e4964adac
Charles Buczel <charles.buczel@gmail.com>
2017-04-25 18:52:18 +0100

    Playing with react

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 1
+    };
+  }
+  incrementCount() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="counter">
+        <h1>{this.state.count}</h1>
+        <button type="button" onClick={() => this.incrementCount()}>Increment</button>
       </div>
     );
   }
+}
+
+App.defaultProps = {
+  name: 'PlaceholderName'
 }
 
 export default App;
